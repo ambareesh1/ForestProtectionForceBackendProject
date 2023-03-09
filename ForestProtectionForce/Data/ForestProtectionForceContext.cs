509b186026b308a66f6bb0546149c4cab19a7969
ambareesh1 <ambru333@gmail.com>
2023-03-09 10:44:27 +0530

    Add project files.

---
+++ ForestProtectionForce/Data/ForestProtectionForceContext.cs
@@ -0,0 +1,41 @@
+﻿ using System;
+using System.Collections.Generic;
+using ForestProtectionForce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForestProtectionForce.Data;
+
+public partial class ForestProtectionForceContext : DbContext
+{
+    public ForestProtectionForceContext(DbContextOptions<ForestProtectionForceContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Test> Tests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Test>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("test");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Address).HasMaxLength(45);
+            entity.Property(e => e.City).HasMaxLength(45);
+            entity.Property(e => e.Name).HasMaxLength(45);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
